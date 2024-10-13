307e18eff955597591b13b2fec4f02845824b920
antonrepositories <antondlyakr@gmail.com>
2024-10-13 14:01:47 +0300

    lab2 updt

--- lab2.pl
+++ lab2.pl
@@ -1,5 +1,3 @@
-:- discontiguous is_a/2.
-:- discontiguous part_of/2.
 % Родова-видові зв'язки (is_a)
 is_a(cat, mammal).
 is_a(dog, mammal).
@@ -15,7 +13,9 @@ is_a(reptile, animal).
 is_a(snake, reptile).
 is_a(crocodile, reptile).
 
-% Зв'язки частина-ціле (part_of)
+is_a(X, Z) :- is_a(X, Y), is_a(Y, Z).
+
+% Зв'язки частина-ціле
 part_of(wing, eagle).
 part_of(beak, eagle).
 part_of(wing, sparrow).
@@ -36,30 +36,8 @@ part_of(tusk, elephant).
 
 part_of(poison, snake).
 part_of(tail, crocodile).
-% Транзитивні зв'язки
-part_of(cat, mammal).
-part_of(dog, mammal).
-part_of(elephant, mammal).
 
-part_of(eagle, bird).
-part_of(sparrow, bird).
-
-part_of(shark, fish).
-part_of(salmon, fish).
-
-part_of(snake, reptile).
-part_of(crocodile, reptile).
-
-part_of(mammal, animal).
-part_of(reptile, animal).
-part_of(bird, animal).
-part_of(fish, animal).
-
-% Правило транзитивності для родо-видових зв'язків (is_a)
-is_a(X, Z) :- is_a(X, Y), is_a(Y, Z).
-
-% Правило транзитивності для зв'язків частина-ціле (part_of)
-part_of(X, Z) :- part_of(X, Y), part_of(Y, Z).
+part_of(X, Z) :- part_of(X, Y), is_a(Y, Z).
 
 % Якщо X є ссавцем, то у нього є шерсть
 has_fur(X) :- is_a(X, mammal).
